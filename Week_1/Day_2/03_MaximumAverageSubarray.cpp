8da491beeeb335c2da4d4d20fa42b6050d1669fa
mdivyanshu009 <mdivyanshu09@gmail.com>
2026-06-02 16:16:01 +0530

    Day 2 Problems Completed

---
+++ Week_1/Day_2/03_MaximumAverageSubarray.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Problem Link- https://leetcode.com/problems/maximum-average-subarray-i/
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n=nums.size();
+        double windowsum=0;
+        for(int i=0;i<k;i++){
+            windowsum+=nums[i];
+        }
+        double maxsum=windowsum;
+        
+        for(int i=k;i<n;i++){
+            windowsum+=nums[i]-nums[i-k];
+            maxsum = max(maxsum, windowsum);
+        }
+        return maxsum/k;
+
+    }
+};
